6446105ed4a0047eb55d91ee3dcb9563479c3d33
Simon Gautier <gautier.simon@gmail.com>
2020-12-04 13:55:52 +0100

    Doctrine : contains, clear, refresh, remove

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -16,7 +16,7 @@ class VehicleController extends AbstractController
     /**
      * @Route("/add", name="vehicle_add")
      */
-    public function indexAction()
+    public function addAction()
     {
         // you can fetch the EntityManager via $this->getDoctrine() or $this->get('doctrine')
         // or you can add an argument to your action: index(EntityManagerInterface $em)
@@ -32,6 +32,37 @@ class VehicleController extends AbstractController
         // actually executes the queries (i.e. the INSERT query)
         $em->flush();
 
-        return new Response('<body>Saved new vehicle with id '.$vehicle->getId().'</body>');
+        return new Response('<body>Saved new vehicle with id ' . $vehicle->getId() . '</body>');
     }
+
+    /**
+     * @Route("/test-entity-manager", name="vehicle_test_entity_manager")
+     */
+    public function testEntityManagerAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $vehicle = new Vehicle();
+        $vehicle->setPlate('AB-123-CD')->setMileage(58000)->setPrice(19999.99)
+            ->setManufactureDate(new \DateTime('2017-11-24'))->setDescription('Très belle voiture');
+        $vehicle2 = clone($vehicle);
+        $em->persist($vehicle);
+
+        if($em->contains($vehicle)) { /* true */ dump('$vehicle1 est connu'); }
+        if($em->contains($vehicle2)) { /* false */ dump('$vehicle2 est connu'); }
+        $em->clear('App\Entity\Vehicle'); // Toutes les entités Vehicle ne seront plus persistées
+        $em->clear(); // Plus aucune entité ne sera persistée (tout type confondu)
+
+        $em->persist($vehicle2);
+        $em->flush();
+
+        $vehicle2->setPlate('BA-321-DC');
+        dump($vehicle2);
+        $em->refresh($vehicle2); // $vehicle2 est rechargé avec ses données en base
+        dump($vehicle2);
+        $em->remove($vehicle2); // Supprimera $vehicle2 au prochain flush()
+        $em->flush();
+
+        return new Response('<body>Aucun changement dans la base de données :)</body>');
+    }
+
 }
