4656b44eb263f8c3aa74d620d679e9fb47f4b1d8
Simon Gautier <gautier.simon@gmail.com>
2020-12-04 13:08:15 +0100

    Doctrine : create Vehicle entity php bin/console make:entity

---
+++ src/Entity/Vehicle.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehicleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VehicleRepository::class)
+ */
+class Vehicle
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $plate;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $mileage;
+
+    /**
+     * @ORM\Column(type="decimal", precision=8, scale=2, nullable=true)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $manufactureDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlate(): ?string
+    {
+        return $this->plate;
+    }
+
+    public function setPlate(string $plate): self
+    {
+        $this->plate = $plate;
+
+        return $this;
+    }
+
+    public function getMileage(): ?int
+    {
+        return $this->mileage;
+    }
+
+    public function setMileage(int $mileage): self
+    {
+        $this->mileage = $mileage;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getManufactureDate(): ?\DateTimeInterface
+    {
+        return $this->manufactureDate;
+    }
+
+    public function setManufactureDate(\DateTimeInterface $manufactureDate): self
+    {
+        $this->manufactureDate = $manufactureDate;
+
+        return $this;
+    }
+}
