ccec8bb16827540d0f992681f96541589bd7c394
Simon Gautier <gautier.simon@gmail.com>
2022-10-06 21:14:13 +0200

    Préfixe sur toutes les routes d'un contrôleur

---
+++ src/Controller/RouteTestBisController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/route-bis', name: 'prefixe_pour_name_')]
+class RouteTestBisController extends AbstractController
+{
+    // En réalité, la route de cette action sera /route-bis/test
+    #[Route('/test', name: 'nom-de-ma-route-bis')]
+    public function testAction(): Response
+    {
+        return new Response('<body>Contenu de la réponse</body>');
+    }
+
+    // En réalité, la route de cette action sera /route-bis/with-variable/{id}
+    #[Route('/with-variable/{id}', name: 'nom-de-ma-route-2-bis')]
+    public function routeWithVariableAction($id): Response
+    {
+        // $id est accessible ici de manière automatique
+        return new Response("<body>$id</body>");
+    }
+
+    // ...
+}
