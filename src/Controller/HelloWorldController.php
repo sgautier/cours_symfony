dc47f317ffc5657b555b50dfa264e310cafa5388
Simon Gautier <gautier.simon@gmail.com>
2022-10-06 19:58:45 +0200

    - Initialisation du projet - Ajout du Maker Bundle - Ajout du Hello World !

---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloWorldController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return new Response('Hello World !');
+    }
+}
