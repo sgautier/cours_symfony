b89786d645b7d214154d7fe1c92d261890d57140
Simon Gautier <gautier.simon@gmail.com>
2022-10-08 17:06:16 +0200

    Doctrine : migration pour ajout de la table de l'entité VehicleSecurity

---
+++ migrations/Version20221008150506.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221008150506 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE vehicle_security (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, euro_ncap_stars INTEGER NOT NULL, airbag_number INTEGER NOT NULL, abs BOOLEAN NOT NULL, esp BOOLEAN NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE vehicle_security');
+    }
+}
