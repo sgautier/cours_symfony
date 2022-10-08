f01dc038515ba3cdb8c67ddc9340d2abdd1165a8
Simon Gautier <gautier.simon@gmail.com>
2022-10-08 11:26:09 +0200

    Doctrine : initialisation entité Vehicle et création migration

---
+++ migrations/Version20221008085639.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221008085639 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE voiture (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, plate VARCHAR(10) NOT NULL, mileage INTEGER NOT NULL, price DOUBLE PRECISION DEFAULT NULL, description CLOB DEFAULT NULL, manu_date DATETIME NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE voiture');
+    }
+}
---
+++ src/Entity/Vehicle.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehicleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VehicleRepository::class)]
+#[ORM\Table(name: 'voiture')]
+class Vehicle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $plate = null;
+
+    #[ORM\Column]
+    private ?int $mileage = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $price = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(name: 'manu_date', type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $manufactureDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlate(): ?string
+    {
+        return $this->plate;
+    }
+
+    public function setPlate(string $plate): self
+    {
+        $this->plate = $plate;
+
+        return $this;
+    }
+
+    public function getMileage(): ?int
+    {
+        return $this->mileage;
+    }
+
+    public function setMileage(int $mileage): self
+    {
+        $this->mileage = $mileage;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getManufactureDate(): ?\DateTimeInterface
+    {
+        return $this->manufactureDate;
+    }
+
+    public function setManufactureDate(\DateTimeInterface $manufactureDate): self
+    {
+        $this->manufactureDate = $manufactureDate;
+
+        return $this;
+    }
+}
