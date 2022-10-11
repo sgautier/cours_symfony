785dd2f7f727f83efeaf79bfe404aaca320a343c
Simon Gautier <gautier.simon@gmail.com>
2022-10-11 18:12:12 +0200

    Paramètres pour les services

---
+++ src/MyClassSomewhere.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class MyClassSomewhere
+{
+    public const MY_CONSTANT = 'Ceci est une constante';
+}
--- src/Service/TestScalar.php
+++ src/Service/TestScalar.php
@@ -8,15 +8,21 @@ class TestScalar
 {
     private string $myString;
     private int $myInt;
+    private string $myConstant;
+    private array $array;
 
-    public function __construct(string $myString, int $myInt)
+    public function __construct(string $myString, int $myInt, string $myConstant, array $array)
     {
         $this->myString = $myString;
         $this->myInt = $myInt;
+        $this->myConstant = $myConstant;
+        $this->array = $array;
     }
 
     public function __toString()
     {
-        return "Values : {$this->myString} - {$this->myInt}";
+        return "Values : {$this->myString} - {$this->myInt} - {$this->myConstant} - "
+            . implode(', ', $this->array)
+            ;
     }
 }
