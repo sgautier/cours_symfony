85aaef9cd8544a314b86aa4b731e22eddc1bd9d8
Simon Gautier <gautier.simon@gmail.com>
2023-10-10 10:56:50 +0200

    Remove obsolete Sensio framework extra bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,6 +12,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
