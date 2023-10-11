5774c2a8c0c905b9c9e79cc203961d4c6bd7069d
Simon Gautier <gautier.simon@gmail.com>
2023-10-11 09:51:37 +0200

    Securité : ajout du contrôleur pour tester les routes /admin/...

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin', name: 'admin_')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function indexAction(): Response
+    {
+        dump($this->getUser());
+        return new Response("<body>Ceci est l'accueil de l'administration</body>");
+    }
+
+    #[Route('/non-protected', name: 'home')]
+    public function homeAction()
+    {
+        dump($this->getUser());
+        return new Response("<body>Ceci est une page non protégée de l'administration</body>");
+    }
+
+    #[Route('/informations', name: 'informations')]
+    public function informationsAction()
+    {
+        dump($this->getUser());
+        return new Response("<body>Ceci est la page où je gère mes informations personnelles</body>");
+    }
+}
