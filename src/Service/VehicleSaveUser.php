6fd99e270fe7ebac6a076f3acd22a6f1efeaddb1
Simon Gautier <gautier.simon@gmail.com>
2023-10-13 13:27:31 +0200

    VehicleSaveUser : revue de code

--- src/Service/VehicleSaveUser.php
+++ src/Service/VehicleSaveUser.php
@@ -5,17 +5,19 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\Vehicle;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Doctrine\ORM\Events;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Doctrine\ORM\Event\LifecycleEventArgs;
 
-class VehicleSaveUser implements EventSubscriber
+#[AsDoctrineListener(event: Events::prePersist, priority: 0)]
+readonly class VehicleSaveUser implements EventSubscriber
 {
-    private TokenStorageInterface $token;
-
-    public function __construct(TokenStorageInterface $token)
+    public function __construct(
+        private TokenStorageInterface $token,
+    )
     {
-        $this->token = $token;
     }
 
     public function getSubscribedEvents(): array
@@ -23,7 +25,7 @@ class VehicleSaveUser implements EventSubscriber
         return ['prePersist',];
     }
 
-    public function prePersist(LifecycleEventArgs $args): void
+    public function prePersist(PrePersistEventArgs $args): void
     {
         $entity = $args->getObject();
 
