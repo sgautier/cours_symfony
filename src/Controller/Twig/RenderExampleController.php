a763809f94a9ff08e9fbce4ae38daf696284f08e
Simon Gautier <gautier.simon@gmail.com>
2024-11-13 17:44:07 +0100

    Typo

--- src/Controller/Twig/RenderExampleController.php
+++ src/Controller/Twig/RenderExampleController.php
@@ -11,7 +11,7 @@ class RenderExampleController extends AbstractController
 {
     public function bestSalesAction($nbProducts): Response
     {
-        // Evidemment, dans la vraie vie, les produits sont chargés depuis la base de données par exemple
+        // Évidemment, dans la vraie vie, les produits sont chargés depuis la base de données par exemple
         $products = [];
         for ($i = 1; $i <= $nbProducts; $i++) {
             $products[] = ['name' => 'Product ' . $i, 'price' => 25 + $i,];
